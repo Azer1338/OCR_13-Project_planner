0f264568ad746c4aa164c8ce7bb9d6dac0d14c2e
Azer1338 <Azergh**1338>
2020-10-12 20:16:20 +0200

    adapt urls

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l(q8x)e%d+upzxh2x5+^8(6adzn(aj+6(y%r8ju!m(#0^54&26'
+SECRET_KEY = 'l(q8x)e%d+upzxh2x5+^8(6adzn(aj+6(y%r8ju!m(#0^8888'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
