7014c4e5f2618977266530cf4a7ba6cb514da964
Azer1338 <Azergh**1338>
2020-10-12 20:38:14 +0200

    install apps

--- main/settings.py
+++ main/settings.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import environs
+
+
+# Set up Environs lib
+env = environs.Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,23 +27,41 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l(q8x)e%d+upzxh2x5+^8(6adzn(aj+6(y%r8ju!m(#0^8888'
+SECRET_KEY = os.environ.get('SECRET_KEY', env.str("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ENV = env.str("ENV")
 
-ALLOWED_HOSTS = []
+if os.environ.get('ENV') == 'PRODUCTION':
+    DEBUG = False
+else:
+    DEBUG = True
+
+# Allow the access to
+if os.environ.get('ENV') == 'PRODUCTION':
+    ALLOWED_HOSTS = ['***********']
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
+AUTH_USER_MODEL = 'account.ProjectPlannerUser'
 
+
+# Application definition
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
+    'project.apps.ProjectConfig',
+    'document.apps.DocumentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'crispy_forms',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
@@ -47,9 +72,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -72,18 +99,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # on utilise l'adaptateur postgresql
+        'ENGINE': 'django.db.backends.postgresql',
+        # le nom de notre base de donnees creee precedemment
+        'NAME': env.str('YOUR_DATABASE_NAME'),
+        # attention : remplacez par votre nom d'utilisateur
+        'USER': env.str('YOUR_USER_DB'),
+        'PASSWORD': env.str('YOUR_PASSWORD_DB'),
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -102,19 +134,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'fr'
+TIME_ZONE = 'Europe/Paris'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
