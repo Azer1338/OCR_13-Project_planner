969f793a4ce0627c205fa60bd07fda54be008364
Azer1338 <Azergh**1338>
2020-11-03 16:48:15 +0100

    add some section into the admin page

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from project.models import Project, Deliverable
+
+
+@admin.register(Deliverable)
+class DeliverableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'addingDate', 'status', 'progression', 'project')
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creationDate', 'status', 'advancement')
+
